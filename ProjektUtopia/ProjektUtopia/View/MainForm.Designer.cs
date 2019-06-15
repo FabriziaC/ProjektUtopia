7638e2fa1da110d19eb1bfa9680bc73d4a3b399f
gianluca.carrivale <gianluca.carrivale@gmail.com>
2019-06-15 17:54:52 +0200

    Bugfix

--- ProjektUtopia/ProjektUtopia/View/MainForm.Designer.cs
+++ ProjektUtopia/ProjektUtopia/View/MainForm.Designer.cs
@@ -29,13 +29,13 @@
     private void InitializeComponent()
     {
       this.FolderPath_TextBox = new System.Windows.Forms.TextBox();
-      this.SelectedPath_Label = new System.Windows.Forms.Label();
-      this.Run_Button = new System.Windows.Forms.Button();
+      this.label2 = new System.Windows.Forms.Label();
+      this.RunButton = new System.Windows.Forms.Button();
       this.FolderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
-      this.Browse_Button = new System.Windows.Forms.Button();
-      this.DragNDrop_Label = new System.Windows.Forms.Label();
-      this.DragNDrop_Panel = new System.Windows.Forms.Panel();
-      this.DragNDrop_Panel.SuspendLayout();
+      this.BrowseButton = new System.Windows.Forms.Button();
+      this.label1 = new System.Windows.Forms.Label();
+      this.DragNDropPanel = new System.Windows.Forms.Panel();
+      this.DragNDropPanel.SuspendLayout();
       this.SuspendLayout();
       // 
       // FolderPath_TextBox
@@ -46,60 +46,60 @@
       this.FolderPath_TextBox.Size = new System.Drawing.Size(504, 20);
       this.FolderPath_TextBox.TabIndex = 2;
       // 
-      // SelectedPath_Label
+      // label2
       // 
-      this.SelectedPath_Label.AutoSize = true;
-      this.SelectedPath_Label.Location = new System.Drawing.Point(12, 146);
-      this.SelectedPath_Label.Name = "SelectedPath_Label";
-      this.SelectedPath_Label.Size = new System.Drawing.Size(68, 13);
-      this.SelectedPath_Label.TabIndex = 4;
-      this.SelectedPath_Label.Text = "Selected file:";
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(12, 146);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(68, 13);
+      this.label2.TabIndex = 4;
+      this.label2.Text = "Selected file:";
       // 
-      // Run_Button
+      // RunButton
       // 
-      this.Run_Button.Location = new System.Drawing.Point(522, 160);
-      this.Run_Button.Name = "Run_Button";
-      this.Run_Button.Size = new System.Drawing.Size(75, 23);
-      this.Run_Button.TabIndex = 5;
-      this.Run_Button.Text = "Run";
-      this.Run_Button.UseVisualStyleBackColor = true;
-      this.Run_Button.Click += new System.EventHandler(this.RunButton_Click);
+      this.RunButton.Location = new System.Drawing.Point(522, 160);
+      this.RunButton.Name = "RunButton";
+      this.RunButton.Size = new System.Drawing.Size(75, 23);
+      this.RunButton.TabIndex = 5;
+      this.RunButton.Text = "Run";
+      this.RunButton.UseVisualStyleBackColor = true;
+      this.RunButton.Click += new System.EventHandler(this.RunButton_Click);
       // 
-      // Browse_Button
+      // BrowseButton
       // 
-      this.Browse_Button.Location = new System.Drawing.Point(248, 64);
-      this.Browse_Button.Name = "Browse_Button";
-      this.Browse_Button.Size = new System.Drawing.Size(75, 23);
-      this.Browse_Button.TabIndex = 1;
-      this.Browse_Button.Text = "browse";
-      this.Browse_Button.UseVisualStyleBackColor = true;
-      this.Browse_Button.Click += new System.EventHandler(this.BrowseButton_Click);
+      this.BrowseButton.Location = new System.Drawing.Point(248, 64);
+      this.BrowseButton.Name = "BrowseButton";
+      this.BrowseButton.Size = new System.Drawing.Size(75, 23);
+      this.BrowseButton.TabIndex = 1;
+      this.BrowseButton.Text = "browse.";
+      this.BrowseButton.UseVisualStyleBackColor = true;
+      this.BrowseButton.Click += new System.EventHandler(this.BrowseButton_Click);
       // 
-      // DragNDrop_Label
+      // label1
       // 
-      this.DragNDrop_Label.AutoSize = true;
-      this.DragNDrop_Label.Location = new System.Drawing.Point(220, 34);
-      this.DragNDrop_Label.Name = "DragNDrop_Label";
-      this.DragNDrop_Label.Size = new System.Drawing.Size(134, 13);
-      this.DragNDrop_Label.TabIndex = 3;
-      this.DragNDrop_Label.Text = "Drag and drop your files or ";
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(220, 34);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(134, 13);
+      this.label1.TabIndex = 3;
+      this.label1.Text = "Drag and drop your files or ";
       // 
-      // DragNDrop_Panel
+      // DragNDropPanel
       // 
-      this.DragNDrop_Panel.AllowDrop = true;
-      this.DragNDrop_Panel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+      this.DragNDropPanel.AllowDrop = true;
+      this.DragNDropPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-      this.DragNDrop_Panel.AutoSize = true;
-      this.DragNDrop_Panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-      this.DragNDrop_Panel.Controls.Add(this.DragNDrop_Label);
-      this.DragNDrop_Panel.Controls.Add(this.Browse_Button);
-      this.DragNDrop_Panel.Location = new System.Drawing.Point(15, 12);
-      this.DragNDrop_Panel.Name = "DragNDrop_Panel";
-      this.DragNDrop_Panel.Size = new System.Drawing.Size(582, 115);
-      this.DragNDrop_Panel.TabIndex = 4;
-      this.DragNDrop_Panel.DragDrop += new System.Windows.Forms.DragEventHandler(this.DragDrop_Event);
-      this.DragNDrop_Panel.DragOver += new System.Windows.Forms.DragEventHandler(this.DragOver_Event);
+      this.DragNDropPanel.AutoSize = true;
+      this.DragNDropPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+      this.DragNDropPanel.Controls.Add(this.label1);
+      this.DragNDropPanel.Controls.Add(this.BrowseButton);
+      this.DragNDropPanel.Location = new System.Drawing.Point(15, 12);
+      this.DragNDropPanel.Name = "DragNDropPanel";
+      this.DragNDropPanel.Size = new System.Drawing.Size(582, 115);
+      this.DragNDropPanel.TabIndex = 4;
+      this.DragNDropPanel.DragDrop += new System.Windows.Forms.DragEventHandler(this.DragDrop_Event);
+      this.DragNDropPanel.DragOver += new System.Windows.Forms.DragEventHandler(this.DragOver_Event);
       // 
       // MainForm
       // 
@@ -107,9 +107,9 @@
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(609, 194);
-      this.Controls.Add(this.DragNDrop_Panel);
-      this.Controls.Add(this.Run_Button);
-      this.Controls.Add(this.SelectedPath_Label);
+      this.Controls.Add(this.DragNDropPanel);
+      this.Controls.Add(this.RunButton);
+      this.Controls.Add(this.label2);
       this.Controls.Add(this.FolderPath_TextBox);
       this.MaximizeBox = false;
       this.MaximumSize = new System.Drawing.Size(625, 233);
@@ -118,8 +118,8 @@
       this.Name = "MainForm";
       this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
       this.Text = "Projekt Utopia";
-      this.DragNDrop_Panel.ResumeLayout(false);
-      this.DragNDrop_Panel.PerformLayout();
+      this.DragNDropPanel.ResumeLayout(false);
+      this.DragNDropPanel.PerformLayout();
       this.ResumeLayout(false);
       this.PerformLayout();
 
@@ -127,12 +127,12 @@
 
     #endregion
     private System.Windows.Forms.TextBox FolderPath_TextBox;
-    private System.Windows.Forms.Label SelectedPath_Label;
-    private System.Windows.Forms.Button Run_Button;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Button RunButton;
     private System.Windows.Forms.FolderBrowserDialog FolderBrowserDialog;
-    private System.Windows.Forms.Button Browse_Button;
-    private System.Windows.Forms.Label DragNDrop_Label;
-    private System.Windows.Forms.Panel DragNDrop_Panel;
+    private System.Windows.Forms.Button BrowseButton;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Panel DragNDropPanel;
   }
 }
 
